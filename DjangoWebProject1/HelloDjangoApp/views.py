eb87c2cc82f2dafdf4dd4a1d297f3bf23f60fe6e
Tashi Tobgay <0624012@schoolsnet.act.edu.au>
2026-02-24 10:16:40 +1100

    remove comment

---
+++ DjangoWebProject1/HelloDjangoApp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+from datetime import datetime 
+
+# Create your views here.
+def index(request):
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html", 
+        { 
+            'title' : "Django home",
+            'message' : "hello Django",
+            'content' : "on" + now.strftime("%A, %d %B, %Y at %X")
+        }
+ ) 
+   
